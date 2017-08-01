222cb89ca0a5ddcea36671d3df6da84f8c9f81fb
qq <qq@qq.com>
2017-08-01 20:47:38 +0800

    i

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
-//ob_start();
-define('ENV',"dev"); //上线开关
-if(ENV == "dev")	  define('SUFFIX',"_dev");
-elseif(ENV == "test") define('SUFFIX',"_test");
-else                  define('SUFFIX',"");
+$http_host = $_SERVER['HTTP_HOST'];
+if(filter_var($http_host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false){
+    $domain = $http_host;
+}else{
+    $arr = explode('.',$http_host);
+    $c = count($arr);
+    $domain = $arr[$c-2].'.'.$arr[$c-1];
+}
+define('DOMAIN', $domain);
+if(file_exists("pro.txt")){
+    define("CONF_ENV","pro");
+}elseif(file_exists("test.txt")){
+    define("CONF_ENV","test");
+}else{
+    define("CONF_ENV","dev");
+}
 
 define('ROOT',__DIR__);
 define('APP_DEBUG',true);
